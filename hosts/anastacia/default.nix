90ca4637317ce32a021b94c760686665627421b4
Anthony Rodriguez <anthony@nezia.dev>
2024-10-24 14:12:11 +0200

    hosts/anastacia: add laptop ssh key

--- hosts/anastacia/default.nix
+++ hosts/anastacia/default.nix
@@ -14,6 +14,9 @@ in {
   networking.hostName = "anastacia";
   networking.domain = "";
   services.openssh.enable = true;
-  users.users.root.openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEzs7SQH0Vjt9JHoXXmWy9fPU1I3rrRWV5magZFrI5al''];
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEzs7SQH0Vjt9JHoXXmWy9fPU1I3rrRWV5magZFrI5al"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKEPlN/GU9nJZPleA77HH5NA+6vyhhM84fTSjEwnEgq"
+  ];
   system.stateVersion = "23.11";
 }
