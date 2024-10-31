d033f9affbb800c2bdc3649a924f0e67000541ba
Anthony Rodriguez <anthony@nezia.dev>
2024-10-31 09:21:23 +0100

    system/services: remove extra udev rules (not needed with mcuxpresso's udev rules)

--- system/services/default.nix
+++ system/services/default.nix
@@ -5,7 +5,4 @@
 }: {
   imports = [./docker.nix ./gnupg.nix ./pipewire.nix ./kmscon.nix];
   services.udev.packages = [pkgs.segger-jlink inputs.self.packages.${pkgs.system}.mcuxpresso.ide];
-  services.udev.extraRules = ''
-    SUBSYSTEM=="usb", ATTR{idVendor}=="1fc9", ATTR{idProduct}=="0132", MODE="0666"
-  '';
 }
