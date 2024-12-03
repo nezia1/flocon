c1887a3d0a4e415f1bb412dd0d069b8d77ec5621
Anthony Rodriguez <anthony@nezia.dev>
2024-12-03 09:23:18 +0100

    system/services/forgejo: add catppuccin

--- system/services/forgejo.nix
+++ system/services/forgejo.nix
@@ -1,9 +1,18 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }: let
+  inherit (lib) mkAfter removePrefix removeSuffix;
   srv = config.services.forgejo.settings.server;
+
+  # https://github.com/isabelroses/dotfiles/blob/06f8f70914c8e672541a52563ee624ce2e62adfb/modules/nixos/services/selfhosted/forgejo.nix#L19-L23
+  theme = pkgs.fetchzip {
+    url = "https://github.com/catppuccin/gitea/releases/download/v1.0.1/catppuccin-gitea.tar.gz";
+    sha256 = "et5luA3SI7iOcEIQ3CVIu0+eiLs8C/8mOitYlWQa/uI=";
+    stripRoot = false;
+  };
 in {
   services = {
     forgejo = {
@@ -28,6 +37,20 @@ in {
         federation = {
           ENABLED = true;
         };
+        ui = {
+          DEFAULT_THEME = "catppuccin-mocha-lavender";
+          THEMES = builtins.concatStringsSep "," (
+            ["auto,forgejo-auto,forgejo-dark,forgejo-light,arc-gree,gitea"]
+            ++ (map (name: removePrefix "theme-" (removeSuffix ".css" name)) (
+              builtins.attrNames (builtins.readDir theme)
+            ))
+          );
+        };
+
+        actions = {
+          ENABLED = true;
+          DEFAULT_ACTIONS_URL = "https://code.forgejo.org";
+        };
       };
     };
 
@@ -36,6 +59,20 @@ in {
       virtualHosts."git.nezia.dev".extraConfig = ''
         reverse_proxy * localhost:${toString srv.HTTP_PORT}
       '';
+    };
+  };
+
+  # https://github.com/isabelroses/dotfiles/blob/06f8f70914c8e672541a52563ee624ce2e62adfb/modules/nixos/services/selfhosted/forgejo.nix#L59-L71
+  systemd.services = {
+    forgejo = {
+      preStart = let
+        inherit (config.services.forgejo) stateDir;
+      in
+        mkAfter ''
+          rm -rf ${stateDir}/custom/public/assets
+          mkdir -p ${stateDir}/custom/public/assets
+          ln -sf ${theme} ${stateDir}/custom/public/assets/css
+        '';
     };
   };
 
