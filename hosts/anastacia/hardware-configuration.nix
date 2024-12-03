f79cae38150947c997ce56607edead105ee9aa9f
Anthony Rodriguez <anthony@nezia.dev>
2024-12-03 09:23:09 +0100

    hosts/anastacia: update hardware-configuration to include system

--- hosts/anastacia/hardware-configuration.nix
+++ hosts/anastacia/hardware-configuration.nix
@@ -1,4 +1,8 @@
-{modulesPath, ...}: {
+{
+  lib,
+  modulesPath,
+  ...
+}: {
   imports = [(modulesPath + "/profiles/qemu-guest.nix")];
   boot = {
     loader.grub.device = "/dev/sda";
@@ -9,4 +13,6 @@
     device = "/dev/sda1";
     fsType = "ext4";
   };
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
