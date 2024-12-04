bd58cf8dd785f88cbebb5e763d12fe5eb9e6ed2f
Anthony Rodriguez <anthony@nezia.dev>
2024-12-05 00:05:42 +0100

    hosts/vamos: switch from rose-pine-moon to rose-pine

--- hosts/vamos/modules/theme.nix
+++ hosts/vamos/modules/theme.nix
@@ -5,9 +5,9 @@
       url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/e0cf0eb237dc5baba86661a3572b20a6183c1876/wallpapers/nix-wallpaper-nineish-catppuccin-frappe.png?raw=true";
       hash = "sha256-/HAtpGwLxjNfJvX5/4YZfM8jPNStaM3gisK8+ImRmQ4=";
     };
-    schemeName = "rose-pine-moon";
+    schemeName = "rose-pine";
     gtk.theme = {
-      name = "rose-pine-moon";
+      name = "rose-pine";
       package = pkgs.rose-pine-gtk-theme;
     };
   };
