09c34d14a32f955e54f525d6a1e26456797595bd
Anthony Rodriguez <anthony@nezia.dev>
2024-12-06 08:55:20 +0100

    modules/theme: add fish colors

--- modules/theme/default.nix
+++ modules/theme/default.nix
@@ -268,6 +268,31 @@ in {
             };
           };
 
+          fish.interactiveShellInit = ''
+            set emphasized_text  brcyan   # base1
+            set normal_text      brblue   # base0
+            set secondary_text   brgreen  # base01
+            set background_light black    # base02
+            set background       brblack  # base03
+            set -g fish_color_quote        cyan      # color of commands
+            set -g fish_color_redirection  brmagenta # color of IO redirections
+            set -g fish_color_end          blue      # color of process separators like ';' and '&'
+            set -g fish_color_error        red       # color of potential errors
+            set -g fish_color_match        --reverse # color of highlighted matching parenthesis
+            set -g fish_color_search_match --background=yellow
+            set -g fish_color_selection    --reverse # color of selected text (vi mode)
+            set -g fish_color_operator     green     # color of parameter expansion operators like '*' and '~'
+            set -g fish_color_escape       red       # color of character escapes like '\n' and and '\x70'
+            set -g fish_color_cancel       red       # color of the '^C' indicator on a canceled command
+          '';
+
+          starship.settings = {
+            character = {
+              success_symbol = "[➜](bold green)";
+              error_symbol = "[➜](bold red)";
+            };
+          };
+
           nvf.settings.vim.theme = {
             enable = true;
             name = "base16";
