26845938e77387de88f7f5053065c07a7a95c8d6
Anthony Rodriguez <anthony@nezia.dev>
2024-12-19 18:59:03 +0100

    treewide: update waybar

--- config/home-manager/programs/waybar/default.nix
+++ config/home-manager/programs/waybar/default.nix
@@ -1,183 +1,177 @@
 {
   pkgs,
   lib,
-  osConfig,
-  config,
   ...
-}: let
-  inherit (lib) getExe mkMerge mkIf;
-  inherit (builtins) readFile;
-in {
+}: {
   programs.waybar = {
     enable = true;
     systemd.enable = true;
-    style = readFile ./style.css;
     settings = {
-      mainBar = mkMerge [
-        {
-          layer = "top";
-          position = "top";
-          height = 30;
+      mainBar = {
+        layer = "top";
+        position = "top";
 
-          margin-top = 6;
-          margin-left = 6;
-          margin-right = 6;
-          margin-bottom = 0;
+        modules-left = [
+          "hyprland/window"
+          "pulseaudio"
+          "cpu"
+          "memory"
+          "idle_inhibitor"
+        ];
 
-          modules-left = ["hyprland/workspaces" "hyprland/window"];
-          modules-center = ["group/clock"];
-          modules-right =
-            ["tray" "group/status" "group/power"]
-            ++ lib.optional config.services.swaync.enable "custom/swaync";
+        modules-center = ["hyprland/workspaces"];
+        modules-right = [
+          "custom/swaync"
+          "custom/power"
+          "network"
+          "battery"
+          "tray"
+          "clock"
+        ];
 
-          tray = {
-            icon-size = 16;
-            spacing = 12;
+        tray = {
+          icon-size = 16;
+          spacing = 12;
+        };
+
+        battery = {
+          interval = 10;
+          states = {
+            warning = 30;
+            critical = 15;
           };
+          format = "{icon} {capacity}%";
+          format-charging = " {capacity}% - {time}";
+          format-full = " {capacity}% - Full";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+          max-length = 25;
+        };
 
-          battery = {
-            interval = 10;
-            states = {
-              warning = 30;
-              critical = 15;
-            };
-            format = "{icon} {capacity}%";
-            format-charging = " {capacity}% - {time}";
-            format-full = " {capacity}% - Full";
-            format-icons = [
-              ""
-              ""
-              ""
-              ""
-              ""
-            ];
-            max-length = 25;
-          };
-
-          "pulseaudio" = {
-            format = "{icon} {volume}%";
-            format-bluetooth = "{icon} {volume}%";
-            format-muted = " ";
-            format-icons = {
-              "alsa_output.pci-0000_00_1f.3.analog-stereo" = "";
-              "alsa_output.pci-0000_00_1f.3.analog-stereo-muted" = "";
-              headphone = "";
-              hands-free = "";
-              headset = "";
-              phone = "";
-              phone-muted = "";
-              portable = "";
-              car = "";
-              default = [
-                ""
-                ""
-              ];
-            };
-            scroll-step = 1;
-            on-click = "pavucontrol";
-            ignored-sinks = [
-              "Easy Effects Sink"
-            ];
-          };
-
-          "power-profiles-daemon" = {
-            format-icons = {
-              default = "";
-              performance = "";
-              balanced = "";
-              power-saver = "";
-            };
-          };
-
-          "group/status" = {
-            orientation = "inherit";
-            modules = [
-              "pulseaudio"
-              "power-profiles-daemon"
-              "network"
-              "battery"
+        "pulseaudio" = {
+          format = "{icon} {volume}%";
+          format-bluetooth = "{icon} {volume}%";
+          format-muted = " ";
+          format-icons = {
+            "alsa_output.pci-0000_00_1f.3.analog-stereo" = "";
+            "alsa_output.pci-0000_00_1f.3.analog-stereo-muted" = "";
+            headphone = "";
+            hands-free = "";
+            headset = "";
+            phone = "";
+            phone-muted = "";
+            portable = "";
+            car = "";
+            default = [
+              ""
+              ""
             ];
           };
+          scroll-step = 1;
+          on-click = "pavucontrol";
+          ignored-sinks = [
+            "Easy Effects Sink"
+          ];
+        };
 
-          "group/power" = {
-            orientation = "inherit";
-            modules = [
-              "custom/power"
-            ];
+        "power-profiles-daemon" = {
+          format-icons = {
+            default = "";
+            performance = "";
+            balanced = "";
+            power-saver = "";
           };
+        };
 
-          "group/clock" = {
-            orientation = "inherit";
-            modules = [
-              "clock#time"
-              "clock#date"
-            ];
-          };
+        "custom/power" = {
+          format = "";
+          tooltip = false;
+          on-click = lib.getExe pkgs.wlogout;
+        };
 
-          "custom/power" = {
-            format = "";
-            tooltip = false;
-            on-click = getExe pkgs.wlogout;
-          };
+        "memory" = {
+          interval = 5;
+          format = " {}%";
+          tooltip = true;
+        };
 
-          "clock#date" = {
-            format = "{:%D}";
-            tooltip-format = "<tt><big>{calendar}</big></tt>";
-          };
+        "cpu" = {
+          interval = 5;
+          format = " {usage:2}%";
+          tooltip = true;
+        };
 
-          "clock#time" = {
-            format = "{:%H:%M}";
-            tooltip-format = "{tz_list}";
-            timezones = [
-              osConfig.time.timeZone
-              "US/Eastern"
-            ];
+        "idle_inhibitor" = {
+          format = "{icon}";
+          format-icons = {
+            activated = "";
+            deactivated = "";
           };
+          tooltip = "true";
+        };
 
-          network = {
-            interface = "wlp1s0";
-            format = "{ifname}";
-            format-wifi = "{icon}";
-            format-ethernet = "{ipaddr}/{cidr} 󰊗";
-            format-disconnected = "";
-            tooltip-format = "{ifname} via {gwaddr} 󰊗";
-            tooltip-format-wifi = "{essid} ({signalStrength}%)";
-            tooltip-format-ethernet = "{ifname} ";
-            tooltip-format-disconnected = "Disconnected";
-            max-length = 50;
-            format-icons = [
-              "󰤟"
-              "󰤢"
-              "󰤥"
-              "󰤨"
-            ];
+        "clock" = {
+          format = " {:L%H:%M}";
+          tooltip = true;
+          tooltip-format = "<big>{:%A, %d.%B %Y }</big>\n<tt><small>{calendar}</small></tt>";
+        };
+
+        "network" = {
+          format-icons = [
+            "󰤯"
+            "󰤟"
+            "󰤢"
+            "󰤥"
+            "󰤨"
+          ];
+          format-ethernet = " {bandwidthDownOctets}";
+          format-wifi = "{icon} {signalStrength}%";
+          format-disconnected = "󰤮";
+          tooltip = false;
+        };
+
+        "hyprland/window" = {
+          max-length = 22;
+          separate-outputs = false;
+        };
+
+        "hyprland/workspaces" = {
+          format = "{name}";
+          format-icons = {
+            default = " ";
+            active = " ";
+            urgent = " ";
           };
-        }
-        (mkIf osConfig.programs.niri.enable {
-          "niri/window" = {
-            icon = true;
+          on-scroll-up = "hyprctl dispatch workspace e+1";
+          on-scroll-down = "hyprctl dispatch workspace e-1";
+        };
+
+        "custom/swaync" = {
+          tooltip = false;
+          format = "<big>{icon}</big>";
+          format-icons = {
+            none = "";
+            notification = "<span foreground='red'><sup></sup></span>";
+            dnd-notification = "<span foreground='red'><sup></sup></span>";
+            dnd-none = "";
+            inhibited-notification = "<span foreground='red'><sup></sup></span>";
+            inhibited-none = "";
+            dnd-inhibited-notification = "<span foreground='red'><sup></sup></span>";
+            dnd-inhibited-none = "";
           };
-        })
-        (mkIf config.services.swaync.enable {
-          "custom/swaync" = {
-            format = "<big>{icon}</big>";
-            format-icons = {
-              notification = "󱥁";
-              none = "󰍥";
-              dnd-notification = "󱅮";
-              dnd-none = "󱅯";
-            };
-            max-length = 3;
-            return-type = "json";
-            escape = true;
-            exec-if = "which ${pkgs.swaynotificationcenter}/bin/swaync-client";
-            exec = "${pkgs.swaynotificationcenter}/bin/swaync-client --subscribe-waybar";
-            on-click = "${pkgs.swaynotificationcenter}/bin/swaync-client --toggle-panel --skip-wait";
-            on-click-middle = "${pkgs.swaynotificationcenter}/bin/swaync-client --toggle-dnd --skip-wait";
-            tooltip-format = "󰵚  {} notification(s)";
-          };
-        })
-      ];
+          return-type = "json";
+          escape = true;
+          exec-if = "which ${pkgs.swaynotificationcenter}/bin/swaync-client";
+          exec = "${pkgs.swaynotificationcenter}/bin/swaync-client --subscribe-waybar";
+          on-click = "${pkgs.swaynotificationcenter}/bin/swaync-client --toggle-panel --skip-wait";
+          on-click-middle = "${pkgs.swaynotificationcenter}/bin/swaync-client --toggle-dnd --skip-wait";
+        };
+      };
     };
   };
   home.packages = [
--- shared/nixosModules/style/modules/waybar.nix
+++ shared/nixosModules/style/modules/waybar.nix
@@ -1,15 +1,96 @@
 {
   lib,
-  lib',
   config,
   ...
 }: let
   cfg = config.local.style;
+  betterTransition = "all 0.3s cubic-bezier(.55,-0.68,.48,1.682)";
 in {
   config.home-manager.sharedModules = lib.mkIf cfg.enable [
     {
-      programs.waybar.style =
-        lib'.generateGtkColors lib cfg.scheme.palette;
+      programs.waybar.style = ''
+        * {
+          font-family: "0xProto Nerd Font";
+          font-size: 16px;
+          border-radius: 0px;
+          border: none;
+          min-height: 0px;
+        }
+        window#waybar {
+          background: rgba(0,0,0,0);
+        }
+        #workspaces {
+          color: ${cfg.scheme.palette.base00};
+          background: ${cfg.scheme.palette.base01};
+          margin: 4px 4px;
+          padding: 5px 5px;
+          border-radius: 16px;
+        }
+        #workspaces button {
+          font-weight: bold;
+          padding: 0px 5px;
+          margin: 0px 3px;
+          border-radius: 16px;
+          color: ${cfg.scheme.palette.base00};
+          background: linear-gradient(45deg, ${cfg.scheme.palette.base08}, ${cfg.scheme.palette.base0D});
+          opacity: 0.5;
+          transition: ${betterTransition};
+        }
+        #workspaces button.active {
+          font-weight: bold;
+          padding: 0px 5px;
+          margin: 0px 3px;
+          border-radius: 16px;
+          color: ${cfg.scheme.palette.base00};
+          background: linear-gradient(45deg, ${cfg.scheme.palette.base08}, ${cfg.scheme.palette.base0D});
+          transition: ${betterTransition};
+          opacity: 1.0;
+          min-width: 40px;
+        }
+        #workspaces button:hover {
+          font-weight: bold;
+          border-radius: 16px;
+          color: ${cfg.scheme.palette.base00};
+          background: linear-gradient(45deg, ${cfg.scheme.palette.base08}, ${cfg.scheme.palette.base0D});
+          opacity: 0.8;
+          transition: ${betterTransition};
+        }
+        tooltip {
+          background: ${cfg.scheme.palette.base00};
+          border: 1px solid ${cfg.scheme.palette.base08};
+          border-radius: 12px;
+        }
+        tooltip label {
+          color: ${cfg.scheme.palette.base08};
+        }
+        #window, #pulseaudio, #cpu, #memory, #idle_inhibitor {
+          font-weight: bold;
+          margin: 4px 0px;
+          margin-left: 7px;
+          padding: 0px 18px;
+          background: ${cfg.scheme.palette.base00};
+          color: ${cfg.scheme.palette.base05};
+          border-radius: 24px 10px 24px 10px;
+        }
+        #network, #battery,
+        #custom-swaync, #tray, #custom-power {
+          font-weight: bold;
+          background: ${cfg.scheme.palette.base00};
+          color: ${cfg.scheme.palette.base05};
+          margin: 4px 0px;
+          margin-right: 7px;
+          border-radius: 10px 24px 10px 24px;
+          padding: 0px 18px;
+        }
+        #clock {
+          font-weight: bold;
+          color: ${cfg.scheme.palette.base00};
+          background: linear-gradient(90deg, ${cfg.scheme.palette.base0E}, ${cfg.scheme.palette.base0C});
+          margin: 0px;
+          padding: 0px 15px 0px 30px;
+          border-radius: 0px 0px 0px 40px;
+        }
+      '';
     }
   ];
 }
