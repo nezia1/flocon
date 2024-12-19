ae608542478aa8a0be490bc53e1ccec12a3e4820
Anthony Rodriguez <anthony@nezia.dev>
2024-12-19 19:12:01 +0100

    nixos/programs/games: add proton-ge declaratively

--- config/nixos/programs/games.nix
+++ config/nixos/programs/games.nix
@@ -1,6 +1,12 @@
-{
+{pkgs, ...}: {
   programs = {
-    steam.enable = true;
+    steam = {
+      enable = true;
+      extraCompatPackages = [
+        pkgs.proton-ge-bin
+      ];
+    };
+
     gamemode.enable = true;
     gamescope.enable = true;
 
