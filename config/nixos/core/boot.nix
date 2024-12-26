b10f86de80b6138e93c52dc9842c999021eb106d
Anthony Rodriguez <anthony@nezia.dev>
2024-12-26 17:47:50 +0100

    nixos/core: disable unnecessary flags, use latest kernel

--- config/nixos/core/boot.nix
+++ config/nixos/core/boot.nix
@@ -1,4 +1,4 @@
-_: {
+{pkgs, ...}: {
   boot = {
     loader = {
       timeout = 0;
@@ -23,12 +23,10 @@ _: {
 
     kernelParams = [
       "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
+      "systemd.show_status=auto"
       "rd.udev.log_level=3"
-      "udev.log_priority=3"
     ];
+
+    kernelPackages = pkgs.linuxPackages_latest;
   };
 }
