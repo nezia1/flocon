678a0c007d6cdc8e487bd09a2c0a2cb0868eb4dd
Anthony Rodriguez <anthony@nezia.dev>
2024-12-30 22:45:56 +0100

    nixos/nix/nixpkgs: update segger-link

--- config/nixos/nix/nixpkgs.nix
+++ config/nixos/nix/nixpkgs.nix
@@ -5,7 +5,7 @@
       permittedInsecurePackages = [
         "cinny-4.2.3"
         "cinny-unwrapped-4.2.3"
-        "segger-jlink-qt4-796s"
+        "segger-jlink-qt4-810"
       ];
       segger-jlink.acceptLicense = true;
     };
