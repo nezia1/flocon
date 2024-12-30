9e5f6d2c8538c43220d0f474cc86020f61ef9b3a
Anthony Rodriguez <anthony@nezia.dev>
2024-12-30 21:51:59 +0100

    home-manager/programs/games: remove unneeded programs

--- config/home-manager/programs/games/default.nix
+++ config/home-manager/programs/games/default.nix
@@ -1,23 +1,7 @@
-{
-  pkgs,
-  inputs,
-  ...
-}: {
+{pkgs, ...}: {
   home.packages = [
-    pkgs.bottles
     pkgs.mangohud
-    pkgs.protonplus
-
-    # steamtinkerlaunch dependencies
-    pkgs.wget
-    pkgs.xdotool
-    pkgs.xorg.xrandr
-    pkgs.xorg.xprop
-    pkgs.xorg.xwininfo
-    pkgs.xxd
-    pkgs.yad
-
-    inputs.self.packages.${pkgs.system}.bolt-launcher
+    pkgs.bolt-launcher
     pkgs.ankama-launcher
   ];
 }
