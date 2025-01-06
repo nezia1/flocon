2b89ed8b73025b175069fc2a7fce2bb97c4bcfd2
Anthony Rodriguez <anthony@nezia.dev>
2025-01-06 16:01:58 +0100

    hosts/vamos/config/theme: update wallpaper

--- hosts/vamos/config/theme.nix
+++ hosts/vamos/config/theme.nix
@@ -1,6 +1,9 @@
-_: {
+{pkgs, ...}: {
   local.style = {
     enable = true;
-    wallpaper = ../../../assets/wallpapers/nixos-tan-hypr-chan.png;
+    wallpaper = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/refs/heads/master/wallpapers/nix-wallpaper-simple-dark-gray.png";
+      hash = "sha256-JaLHdBxwrphKVherDVe5fgh+3zqUtpcwuNbjwrBlAok=";
+    };
   };
 }
