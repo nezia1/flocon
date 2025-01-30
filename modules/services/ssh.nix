1fb4a2ec9c8dc2dea824501df6925a3667db32e3
Anthony Rodriguez <anthony@nezia.dev>
2025-01-30 15:05:48 +0100

    services/ssh: add keys to agent automatically

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -2,10 +2,20 @@
   lib,
   config,
   ...
-}: {
+}: let
+  inherit (config.local.systemVars) username;
+in {
   config = lib.mkIf config.local.profiles.desktop.enable {
     programs.ssh = {
       startAgent = true;
+    };
+
+    hjem.users.${username} = {
+      files = {
+        ".ssh/config".text = ''
+          AddKeysToAgent yes
+        '';
+      };
     };
   };
 }
