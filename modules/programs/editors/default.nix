c7fbdb2a3fd0eb7b9d9746de9c18bddb03d385bf
Anthony Rodriguez <anthony@nezia.dev>
2025-02-02 13:42:16 +0100

    programs/editors: fix user EDITOR value being overriden by the default NixOS one

--- modules/programs/editors/default.nix
+++ modules/programs/editors/default.nix
@@ -12,6 +12,13 @@ in {
   ];
 
   config = mkIf config.local.profiles.desktop.enable {
+    /*
+    we don't want the default NixOS EDITOR value, which is nano and will override the `environment.d` setting.
+     we have to unset it like this so that our systemd user variable will take precedence:
+    */
+    environment.extraInit = ''
+      unset -v EDITOR
+    '';
     hjem.users.${username} = {
       files = {
         ".config/environment.d/${userEnvFile}.conf".text = ''
