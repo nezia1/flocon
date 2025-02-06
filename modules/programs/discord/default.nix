4bd08afc915945d6662cbabd19bc98b1af1c1abc
Anthony Rodriguez <anthony@nezia.dev>
2025-02-06 14:57:06 +0100

    programs/discord: move to directory to use canary and apply krisp-patcher

--- modules/programs/discord/default.nix
+++ modules/programs/discord/default.nix
@@ -7,11 +7,25 @@
   inherit (lib) mkIf;
   inherit (config.local.systemVars) username;
   styleCfg = config.local.style;
+  discord = pkgs.discord-canary.override {
+    withOpenASAR = true;
+    withVencord = true;
+  };
+
+  # run once with krisp-patcher ~/.config/discordcanary/*/modules/discord_krisp/discord_krisp.node
+  krisp-patcher = pkgs.writers.writePython3Bin "krisp-patcher" {
+    libraries = with pkgs.python3Packages; [capstone pyelftools];
+    flakeIgnore = [
+      "E501" # line too long (82 > 79 characters)
+      "F403" # ‘from module import *’ used; unable to detect undefined names
+      "F405" # name may be undefined, or defined from star imports: module
+    ];
+  } (builtins.readFile ./krisp-patcher.py);
 in {
   config = mkIf config.local.profiles.desktop.enable {
     hjem.users.${username} = {
-      packages = [pkgs.vesktop];
-      files.".config/vesktop/themes/midnight-base16.css".text = with styleCfg.scheme.palette;
+      packages = [discord krisp-patcher];
+      files.".config/Vencord/themes/midnight-base16.css".text = with styleCfg.scheme.palette;
         mkIf styleCfg.enable ''
           /**
            * @name Midnight-base16
