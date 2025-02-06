76dbbd715b46dfe5b6e98d29d89a8c41da78f3be
Anthony Rodriguez <anthony@nezia.dev>
2025-02-06 15:22:47 +0100

    modules: move nh from core/nix to programs
    This is a user program, it cannot be in core.

---
+++ modules/programs/nh.nix
@@ -0,0 +1,21 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
+  inherit (config.local.profiles) desktop;
+  inherit (config.local.systemVars) username;
+in {
+  config = mkIf desktop.enable {
+    programs.nh = {
+      enable = true;
+      clean = {
+        enable = true;
+        extraArgs = "--keep-since 30d --keep 3";
+      };
+    };
+
+    hjem.users.${username}.environment.sessionVariables.FLAKE = "${config.hjem.users.${username}.directory}/.dotfiles";
+  };
+}
