5a0e7a92f4d3529d8c09baae2c3cbe343851f319
Anthony Rodriguez <anthony@nezia.dev>
2025-02-18 10:19:45 +0100

    core/boot: dont setup systemd-boot on servers

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -1,30 +1,39 @@
-{pkgs, ...}: {
-  boot = {
-    loader = {
-      timeout = 0;
-      systemd-boot = {
-        enable = true;
-        consoleMode = "2";
+{
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
+  inherit (config.local.profiles) server;
+in {
+  config = mkIf (!server.enable) {
+    boot = {
+      loader = {
+        timeout = 0;
+        systemd-boot = {
+          enable = true;
+          consoleMode = "2";
+        };
+        efi.canTouchEfiVariables = true;
       };
-      efi.canTouchEfiVariables = true;
+
+      plymouth = {
+        enable = true;
+        extraConfig = ''
+          [Daemon]
+          DeviceScale=2
+        '';
+      };
+
+      consoleLogLevel = 0;
+      initrd.systemd.enable = true;
+      initrd.verbose = false;
+
+      kernelParams = [
+        "quiet"
+        "systemd.show_status=auto"
+        "rd.udev.log_level=3"
+      ];
     };
-
-    plymouth = {
-      enable = true;
-      extraConfig = ''
-        [Daemon]
-        DeviceScale=2
-      '';
-    };
-
-    consoleLogLevel = 0;
-    initrd.systemd.enable = true;
-    initrd.verbose = false;
-
-    kernelParams = [
-      "quiet"
-      "systemd.show_status=auto"
-      "rd.udev.log_level=3"
-    ];
   };
 }
