b1776d7f228279e5d11c53ba83716a59cedef08a
Anthony Rodriguez <anthony@nezia.dev>
2025-02-27 13:29:58 +0100

    programs/swww: fix services dependency ordering

--- modules/programs/swww.nix
+++ modules/programs/swww.nix
@@ -12,22 +12,26 @@
   inherit (config.local.systemVars) username;
 
   updateWallpaper = pkgs.writeShellScript "wallpaper-change" ''
-    ${swww}/bin/swww img "$(${pkgs.coreutils}/bin/shuf -e ${concatStringsSep " " config.local.style.wallpapers} -n 1)"
+    sleep 0.1 && ${swww}/bin/swww img "$(${pkgs.coreutils}/bin/shuf -e ${concatStringsSep " " config.local.style.wallpapers} -n 1)"
   '';
 in {
   config = mkIf config.local.profiles.desktop.enable {
     hjem.users.${username}.systemd.services = {
-      swww = {
-        name = "swww";
-        description = "Wayland wallpaper daemon";
-        partOf = ["graphical-session.target"];
+      swww-daemon = {
+        script = "${pkgs.swww}/bin/swww-daemon";
+        description = "swww-daemon as systemd service";
         wantedBy = ["graphical-session.target"];
-        serviceConfig = {
-          ExecStart = "${swww}/bin/swww-daemon --format xrgb --no-cache";
-          ExecStartPost = updateWallpaper;
-          ExecStop = "${swww}/bin/swww kill";
-          Restart = "on-failure";
-        };
+        after = ["graphical-session.target"];
+        wants = ["swww-update-wallpaper.service"];
+
+        serviceConfig.Slice = "background-graphical.slice";
+      };
+      swww-update-wallpaper = {
+        script = "${updateWallpaper}";
+        description = "Update wallpaper using SWWW";
+        after = ["swww-daemon.service"];
+
+        serviceConfig.Type = "oneshot";
       };
     };
   };
