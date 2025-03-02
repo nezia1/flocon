c078e32ffd72ad4be754e5d626d72912db84693c
Anthony Rodriguez <anthony@nezia.dev>
2025-03-02 15:04:22 +0100

    programs/hyprland: add glassy blur

--- modules/programs/hypr/land/default.nix
+++ modules/programs/hypr/land/default.nix
@@ -163,8 +163,31 @@ in {
               "col.active_border" = "rgb(${lib.removePrefix "#" styleCfg.scheme.palette.base0E})";
             };
             decoration = {
-              rounding = 10;
-              blur.enabled = true;
+              rounding = 16;
+
+              inactive_opacity = 0.40;
+              active_opacity = 0.90;
+
+              blur = {
+                enabled = true;
+                size = 10;
+                passes = 1;
+                new_optimizations = true;
+                ignore_opacity = true;
+                xray = true;
+                noise = 0;
+                brightness = 0.60;
+              };
+
+              shadow = {
+                enabled = true;
+                range = 32;
+                render_power = 3;
+                ignore_window = true;
+                scale = 1;
+                color = "rgba(00000048)";
+                color_inactive = "rgba(00000028)";
+              };
             };
           }
           // import ./binds.nix lib;
