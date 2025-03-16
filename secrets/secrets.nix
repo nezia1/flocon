f235428da44afe375112b9da7316f94f27008c09
Anthony Rodriguez <anthony@nezia.dev>
2025-03-16 11:00:15 +0100

    secrets: add firefox-sync

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,4 +2,5 @@ let
   anastacia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEplguGeXCbdz++Ry5pwJylmtAMnwtf1+9JoJnCGfw3A root@anastacia";
 in {
   "searx-env-file.age".publicKeys = [anastacia];
+  "firefox-sync.age".publicKeys = [anastacia];
 }
