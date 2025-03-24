b088851f3c0d6aab3a6a1f5136c7c5db4f724e96
Anthony Rodriguez <anthony@nezia.dev>
2025-03-24 21:38:28 +0100

    media: remove tidal-hifi dynamic theming
    Problems

--- modules/programs/media/default.nix
+++ modules/programs/media/default.nix
@@ -9,17 +9,16 @@
   inherit (config.local.systemVars) username;
 in {
   imports = [
-    ./tidal-hifi.nix
     ./zathura.nix
   ];
 
   config = mkIf config.local.profiles.desktop.enable {
-    hjem.users.${username}.packages = [
-      pkgs.gnome-calculator
-      pkgs.gthumb
-      pkgs.spotify
-      pkgs.stremio
-      pkgs.celluloid
+    hjem.users.${username}.packages = with pkgs; [
+      gthumb
+      spotify
+      stremio
+      celluloid
+      tidal-hifi
     ];
   };
 }
