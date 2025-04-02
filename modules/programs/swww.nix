ebbf9025158e77d8793a44b91b8e436d159c0bf9
Anthony Rodriguez <anthony@nezia.dev>
2025-04-02 16:35:04 +0200

    programs/swww: switch to flake package

--- modules/programs/swww.nix
+++ modules/programs/swww.nix
@@ -1,4 +1,5 @@
 {
+  inputs,
   lib,
   pkgs,
   config,
@@ -7,31 +8,35 @@
   inherit (lib.modules) mkIf;
   inherit (lib.strings) concatStringsSep;
 
-  inherit (pkgs) swww;
-
   inherit (config.local.systemVars) username;
+  inherit (inputs.swww.packages.${pkgs.system}) swww;
 
-  updateWallpaper = pkgs.writeShellScript "wallpaper-change" ''
-    sleep 0.1 && ${swww}/bin/swww img "$(${pkgs.coreutils}/bin/shuf -e ${concatStringsSep " " config.local.style.wallpapers} -n 1)"
+  updateWallpaper = pkgs.writeShellScript "update-wallpaper" ''
+    ${swww}/bin/swww img "$(${pkgs.uutils-coreutils-noprefix}/bin/shuf -e ${concatStringsSep " " config.local.style.wallpapers} -n 1)"
   '';
 in {
   config = mkIf config.local.profiles.desktop.enable {
     hjem.users.${username}.systemd.services = {
       swww-daemon = {
-        script = "${pkgs.swww}/bin/swww-daemon";
+        script = "${swww}/bin/swww-daemon --format xrgb --no-cache";
         description = "swww-daemon as systemd service";
         wantedBy = ["graphical-session.target"];
         after = ["graphical-session.target"];
         wants = ["swww-update-wallpaper.service"];
-
+        unitConfig.ConditionEnvironment = "WAYLAND_DISPLAY";
         serviceConfig.Slice = "background-graphical.slice";
       };
+
       swww-update-wallpaper = {
         script = "${updateWallpaper}";
         description = "Update wallpaper using SWWW";
         after = ["swww-daemon.service"];
+        requires = ["swww-daemon.service"];
 
-        serviceConfig.Type = "oneshot";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStartPre = "${pkgs.uutils-coreutils-noprefix}/bin/sleep 1";
+        };
       };
     };
   };
