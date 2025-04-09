d7babe5aa3998d4c3270e7def982375e838ba941
Anthony Rodriguez <anthony@nezia.dev>
2025-04-09 09:33:10 +0200

    programs/git: switch to hjem

--- modules/programs/terminal/programs/git.nix
+++ modules/programs/terminal/programs/git.nix
@@ -8,25 +8,27 @@
 
   inherit (config.local.homeVars) signingKey;
   inherit (config.local.systemVars) username;
-
-  toINI = lib.generators.toINI {};
 in {
   config = mkIf config.local.profiles.desktop.enable {
     hjem.users.${username} = {
-      packages = with pkgs; [git gh lazygit];
-      files = {
-        ".config/git/config".text = toINI {
-          user = {
-            name = "Anthony Rodriguez";
-            email = "anthony@nezia.dev";
-            signingkey = signingKey;
+      packages = with pkgs; [gh lazygit];
+      rum = {
+        programs.git = {
+          enable = true;
+          settings = {
+            user = {
+              name = "Anthony Rodriguez";
+              email = "anthony@nezia.dev";
+              signingkey = signingKey;
+            };
+            init.defaultBranch = "main";
+            commit.gpgsign = true;
+            tag.gpgsign = true;
+            gpg.format = "ssh";
+            push.autoSetupRemote = true;
+            pull.rebase = true;
+            merge.conflictstyle = "diff3";
           };
-          init.defaultBranch = "main";
-          commit.gpgsign = true;
-          tag.gpgsign = true;
-          gpg.format = "ssh";
-          push.autoSetupRemote = true;
-          pull.rebase = true;
         };
       };
     };
