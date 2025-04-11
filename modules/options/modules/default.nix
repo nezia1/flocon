570e3a3959489f12d33db903f4186cd25b73bb42
Anthony Rodriguez <anthony@nezia.dev>
2025-04-12 00:08:34 +0200

    options/modules: remove hyprland

--- modules/options/modules/default.nix
+++ modules/options/modules/default.nix
@@ -1,6 +1,5 @@
 {
   imports = [
-    ./hyprland.nix
     ./nvidia.nix
   ];
 }
--- modules/options/modules/hyprland.nix
+++
@@ -1,7 +0,0 @@
-{lib, ...}: let
-  inherit (lib) mkEnableOption;
-in {
-  options.local.modules.hyprland = {
-    enable = mkEnableOption "Hyprland modules";
-  };
-}
