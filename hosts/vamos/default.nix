f194459511ae993ee6060ae6c19f83e2d9cf7b39
Anthony Rodriguez <anthony@nezia.dev>
2025-04-12 00:08:48 +0200

    hosts/vamos: conform to new options API

--- hosts/vamos/default.nix
+++ hosts/vamos/default.nix
@@ -11,15 +11,11 @@ _: {
       fullName = "Anthony Rodriguez";
       email = "anthony@nezia.dev";
       signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKEPlN/GU9nJZPleA77HH5NA+6vyhhM84fTSjEwnEgq nezia@vamos";
+      desktop = "Hyprland";
     };
 
     profiles = {
-      desktop.enable = true;
       laptop.enable = true;
-    };
-
-    modules = {
-      hyprland.enable = true;
     };
 
     style = {
