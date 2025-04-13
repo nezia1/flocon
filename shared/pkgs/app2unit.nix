5281b6e49a62713d65fb3fa28c07bb25797b642f
Anthony Rodriguez <anthony@nezia.dev>
2025-04-13 21:51:28 +0200

    pkgs/app2unit: init app2unit

---
+++ shared/pkgs/app2unit.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  stdenvNoCC,
+  npins,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "app2unit";
+  version = "0-unstable-${npins.app2unit.revision}";
+
+  src = npins.app2unit;
+
+  installPhase = ''
+    install -Dt $out/bin app2unit
+    ln -s $out/bin/app2unit $out/bin/app2unit-open
+  '';
+
+  meta = {
+    description = "Launches Desktop Entries as Systemd user units";
+    homepage = "https://github.com/Vladimir-csp/app2unit";
+    mainProgram = "app2unit";
+    license = lib.licenses.gpl3;
+    platforms = lib.platforms.linux;
+  };
+}
--- shared/pkgs/default.nix
+++ shared/pkgs/default.nix
@@ -1,3 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  npins,
+  ...
+}: {
+  app2unit = pkgs.callPackage ./app2unit.nix {inherit npins;};
   mcuxpresso-udev = pkgs.callPackage ./mcuxpresso-udev.nix {};
 }
