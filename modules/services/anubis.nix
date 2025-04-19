045f0e9877daa96e0ae12708d88ef3756c6d584e
Anthony Rodriguez <anthony@nezia.dev>
2025-04-20 00:33:40 +0200

    services/anubis: init

---
+++ modules/services/anubis.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
+in {
+  config = mkIf config.local.profiles.server.enable {
+    services.anubis = {
+      defaultOptions = {
+        botPolicy = {dnsbl = false;};
+        settings = {
+          DIFFICULTY = 3;
+          SERVE_ROBOTS_TXT = true;
+        };
+      };
+    };
+  };
+}
--- modules/services/default.nix
+++ modules/services/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./swaync
 
+    ./anubis.nix
     ./brightness.nix
     ./caddy.nix
     ./docker.nix
