0353eeba60a1ef990d43cf32556e16334de1f6d9
Anthony Rodriguez <anthony@nezia.dev>
2025-04-20 01:15:16 +0200

    programs/games: manage proton declaratively; configure gamescope

--- modules/programs/games.nix
+++ modules/programs/games.nix
@@ -14,18 +14,24 @@ in {
         pkgs.bolt-launcher
         pkgs.lutris
         pkgs.qbittorrent
-        pkgs.protonplus
       ];
     };
 
     programs = {
       steam = {
         enable = true;
+        gamescopeSession.enable = true;
+        extraCompatPackages = [pkgs.proton-ge-bin];
       };
 
-      gamemode.enable = true;
-      gamescope.enable = true;
-
+      gamescope = {
+        enable = true;
+        capSysNice = true;
+        args = [
+          "--rt"
+          "--expose-wayland"
+        ];
+      };
       coolercontrol = {
         enable = true;
         nvidiaSupport = true;
