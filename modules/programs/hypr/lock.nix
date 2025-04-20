47406c40ca1d4b9636d573ceeee7220ab048c447
Anthony Rodriguez <anthony@nezia.dev>
2025-04-21 01:08:17 +0200

    programs/hyprlock: simplify base16 theming
    Looks better too.

--- modules/programs/hypr/lock.nix
+++ modules/programs/hypr/lock.nix
@@ -39,81 +39,13 @@ in {
                 }
               ];
 
-              label = [
-                {
-                  monitor = "";
-                  text = "Layout: $LAYOUT";
-                  font_size = 25;
-                  color = base05;
-
-                  position = "30, -30";
-                  halign = "left";
-                  valign = "top";
-                }
-                {
-                  monitor = "";
-                  text = "$TIME";
-                  font_size = 90;
-                  color = base05;
-
-                  position = "-30, 0";
-                  halign = "right";
-                  valign = "top";
-                }
-                {
-                  monitor = "";
-                  text = "cmd[update:43200000] date +\"%A, %d %B %Y\"";
-                  font_size = 25;
-                  color = base05;
-
-                  position = "-30, -150";
-                  halign = "right";
-                  valign = "top";
-                }
-              ];
-
-              image = {
-                monitor = "";
-                path = "${styleCfg.avatar}"; # Replace with your avatar path
-                size = 100;
-                border_color = base0D;
-
-                position = "0, 75";
-                halign = "center";
-                valign = "center";
+              input-field = {
+                outer_color = "${base03}";
+                inner_color = "${base00}";
+                font_color = "${base05}";
+                fail_color = "${base08}";
+                check_color = "${base0A}";
               };
-
-              input-field = [
-                {
-                  monitor = "";
-
-                  size = "300, 60";
-                  outline_thickness = 4;
-                  dots_size = 0.2;
-                  dots_spacing = 0.2;
-                  dots_center = true;
-
-                  outer_color = base0D;
-                  inner_color = base02;
-                  font_color = base05;
-
-                  fade_on_empty = false;
-
-                  # the span elements still use #RRGGBB, so we use scheme directly
-                  placeholder_text = "<span foreground=\"#${styleCfg.colors.scheme.palette.base05}\"><i>󰌾 Logged in as </i><span foreground=\"#${styleCfg.colors.scheme.palette.base0D}\">$USER</span></span>";
-
-                  hide_input = false;
-                  check_color = base0D;
-                  fail_color = base08;
-
-                  fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
-                  capslock_color = base0E;
-
-                  position = "0, -47";
-                  halign = "center";
-                  valign = "center";
-                }
-              ];
             }));
     };
   };
