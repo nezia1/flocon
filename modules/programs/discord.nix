39637ebc85eebfec959bf553f448f1733e3d9901
Anthony Rodriguez <anthony@nezia.dev>
2025-04-21 11:39:22 +0200

    programs/discord: avoid IFD
    Instead of doing what we did before, i.e. `builtins.readFile` the output
    path to get the css file, we use `pkgs.concatTextFile` so that no store
    object is realised unnecessarily.

--- modules/programs/discord.nix
+++ modules/programs/discord.nix
@@ -9,40 +9,40 @@
   inherit (config.local.vars.system) username;
   styleCfg = config.local.style;
   discord = pkgs.vesktop;
+
+  base16-discord = pkgs.concatTextFile {
+    name = "base16-discord.css";
+    files = with styleCfg.colors.scheme.palette; [
+      "${npins.base16-discord}/base16.css"
+      (builtins.toFile "base16.css"
+        ''
+          :root {
+              --base00: ${base00};
+              --base01: ${base01};
+              --base02: ${base02};
+              --base03: ${base03};
+              --base04: ${base04};
+              --base05: ${base05};
+              --base06: ${base06};
+              --base07: ${base07};
+              --base08: ${base08};
+              --base09: ${base09};
+              --base0A: ${base0A};
+              --base0B: ${base0B};
+              --base0C: ${base0C};
+              --base0D: ${base0D};
+              --base0E: ${base0E};
+              --base0F: ${base0F};
+          }
+        '')
+    ];
+  };
 in {
   config = mkIf (config.local.vars.home.desktop != "none") {
     hjem.users.${username} = {
       packages = [discord];
       rum.xdg.autostart.programs = [discord];
-      files.".config/vesktop/themes/base16.css".text = with styleCfg.colors.scheme.palette;
-        mkIf styleCfg.enable
-        (
-          (builtins.readFile "${npins.base16-discord.outPath}/base16.css")
-          +
-          /*
-          css
-          */
-          ''
-            :root {
-                --base00: ${base00};
-                --base01: ${base01};
-                --base02: ${base02};
-                --base03: ${base03};
-                --base04: ${base04};
-                --base05: ${base05};
-                --base06: ${base06};
-                --base07: ${base07};
-                --base08: ${base08};
-                --base09: ${base09};
-                --base0A: ${base0A};
-                --base0B: ${base0B};
-                --base0C: ${base0C};
-                --base0D: ${base0D};
-                --base0E: ${base0E};
-                --base0F: ${base0F};
-            }
-          ''
-        );
+      files.".config/vesktop/themes/base16.css".source = mkIf styleCfg.enable base16-discord;
     };
   };
 }
