66c1cd4e2677f214575e616dc0b5cbd55ea91ec3
Anthony Rodriguez <anthony@nezia.dev>
2025-04-21 02:26:04 +0200

    programs/wlogout: theme with base16

--- modules/programs/wlogout.nix
+++ modules/programs/wlogout.nix
@@ -4,22 +4,24 @@
   config,
   ...
 }: let
-  inherit (builtins) concatStringsSep map;
+  inherit (builtins) concatStringsSep map toJSON;
   inherit (lib.meta) getExe';
   inherit (lib.modules) mkIf;
+  inherit (lib.strings) concatMapStringsSep;
 
   inherit (config.local.vars.system) username;
 
-  mkLayout = items: let
-    formatItem = item: ''
-      {
-          "label" : "${item.label}",
-          "action" : "${item.action}", 
-          "text" : "${item.text}",
-          "keybind" : "${item.keybind}"
-      }'';
-  in
-    concatStringsSep "\n" (map formatItem items);
+  styleCfg = config.local.style;
+
+  mkLayout = items:
+    concatStringsSep "\n" (map toJSON items);
+
+  # thanks https://github.com/fufexan/dotfiles/blob/eb3f1cc008b2410aed885b9462e4fbfe47f1bb8e/home/programs/wayland/wlogout.nix#L6-L10
+  bgImageSection = name: ''
+    #${name} {
+      background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/${name}.png"));
+    }
+  '';
 in {
   config = mkIf (config.local.vars.home.desktop == "Hyprland") {
     hjem.users.${username} = {
@@ -73,6 +75,43 @@ in {
               text = "Reboot";
             }
           ];
+
+        ".config/wlogout/style.css".text = with styleCfg.colors.scheme.palette;
+          mkIf styleCfg.enable
+          /*
+          css
+          */
+          ''
+            window {
+              font-size: 18px;
+              background-color: alpha(${base00}, 0.8);
+              color: ${base05};
+            }
+            button {
+              background-repeat: no-repeat;
+              background-position: center;
+              background-size: 25%;
+              border: none;
+              background-color: alpha(${base00}, 0);
+              color: ${base0E};
+            }
+            button:hover {
+              background-color: alpha(${base02}, 0.1);
+            }
+            button:focus {
+              background-color: ${base0E};
+              color: ${base00};
+            }
+
+            ${concatMapStringsSep "\n" bgImageSection [
+              "lock"
+              "logout"
+              "suspend"
+              "hibernate"
+              "shutdown"
+              "reboot"
+            ]}
+          '';
       };
     };
   };
