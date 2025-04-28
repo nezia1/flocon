9af2c8d60db3f77f6634863170d593f502574e90
Anthony Rodriguez <anthony@nezia.dev>
2025-04-28 12:50:22 +0200

    shell/starship: add more git status symbols

--- modules/programs/terminal/shell/starship.nix
+++ modules/programs/terminal/shell/starship.nix
@@ -15,11 +15,19 @@ in {
           error_symbol = "[✖](bold red)";
         };
 
+        # from NotAShelf - https://github.com/NotAShelf/nyx/blob/d407b4d6e5ab7f60350af61a3d73a62a5e9ac660/homes/notashelf/programs/terminal/shell/starship.nix#L76-L88
         git_status = {
-          deleted = "✗";
-          modified = "✶";
-          staged = "✓";
+          style = "red";
+          ahead = "⇡ ";
+          behind = "⇣ ";
+          conflicted = " ";
+          renamed = "»";
+          deleted = "✘ ";
+          diverged = "⇆ ";
+          modified = "!";
           stashed = "≡";
+          staged = "+";
+          untracked = "?";
         };
 
         nix_shell = {
