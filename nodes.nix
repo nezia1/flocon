dd565e813092637ca9dd47dd044452a004336c67
Anthony Rodriguez <anthony@nezia.dev>
2025-04-29 14:28:16 +0200

    nodes: update to use ipv4 as hostname

--- nodes.nix
+++ nodes.nix
@@ -1,9 +1,8 @@
 {inputs, ...}: {
   anastacia = {
-    hostname = "2a01:4f8:1c1c:8495::1";
+    hostname = "78.47.146.254";
     profiles.system = {
       sshUser = "root";
-      user = "root";
       path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos inputs.self.nixosConfigurations.anastacia;
     };
   };
