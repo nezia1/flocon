9c76d33ebe5756ef6b1df5c6173e6150d6e1fc2e
Anthony Rodriguez <anthony@nezia.dev>
2025-04-30 23:15:59 +0200

    hosts/vamos: add wallpaper with `fetchurl`
    So we don't have to fetch the entirety of nixos-artwork everytime.

--- hosts/vamos/default.nix
+++ hosts/vamos/default.nix
@@ -1,4 +1,9 @@
-_: {
+{pkgs, ...}: let
+  wallpaper = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/4ad062cee62116f6055e2876e9638e7bb399d219/wallpapers/nix-wallpaper-nineish-catppuccin-frappe-alt.svg";
+    hash = "sha256-fd2s7fEi1lzrrbYtD5oxCWffVTsIWWlxB8jsbpnn16k=";
+  };
+in {
   imports = [./hardware-configuration.nix];
 
   local = {
@@ -20,6 +25,7 @@ _: {
 
     style = {
       enable = true;
+      wallpapers = [wallpaper];
     };
   };
 }
