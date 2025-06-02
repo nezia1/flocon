9e383401972ade8a9d1895f0c240e51ed3a2ed9e
Anthony Rodriguez <anthony@nezia.dev>
2025-06-03 00:35:40 +0200

    services/gnome: enable gcr from nixpkgs fork
    We make sure to use the package attribute here, to avoid building from
    source (gcr_4 always provided the necessary components)

--- modules/services/gnome.nix
+++ modules/services/gnome.nix
@@ -1,4 +1,5 @@
 {
+  inputs,
   lib,
   pkgs,
   config,
@@ -6,6 +7,8 @@
 }: let
   inherit (lib.modules) mkIf;
 in {
+  # TODO: remove when https://github.com/NixOS/nixpkgs/pull/379731 gets merged
+  imports = ["${inputs.nixpkgs-gcr}/nixos/modules/services/desktops/gnome/gcr-ssh-agent.nix"];
   config = mkIf (config.local.vars.home.desktop == "Hyprland") {
     services = {
       # needed for GNOME services outside of GNOME Desktop
@@ -13,7 +16,14 @@ in {
         gnome-settings-daemon
       ];
 
-      gnome.gnome-keyring.enable = true;
+      gnome = {
+        gnome-keyring.enable = true;
+        gcr-ssh-agent = {
+          enable = true;
+          # TODO: remove when https://github.com/NixOS/nixpkgs/pull/379731 gets merged
+          package = pkgs.gcr_4; # use nixos-unstable gcr_4 to avoid building
+        };
+      };
     };
     programs.seahorse.enable = true;
   };
