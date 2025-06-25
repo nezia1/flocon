4c905e4477562aa7f225a84ebbe6e77d195a1f39
Anthony Rodriguez <anthony@nezia.dev>
2025-06-26 01:05:59 +0200

    options/style: remove MoreWaita patch; switch to mkPackageOption

--- modules/options/style/default.nix
+++ modules/options/style/default.nix
@@ -3,14 +3,12 @@
   pkgs,
   config,
   options,
-  pins,
   inputs',
   ...
 }: let
   inherit (builtins) pathExists toString;
-  inherit (lib.lists) singleton;
   inherit (lib.modules) mkIf;
-  inherit (lib.options) mkEnableOption mkOption;
+  inherit (lib.options) mkEnableOption mkOption mkPackageOption;
   inherit (lib.types) listOf bool package path str;
 
   cfg = config.local.style;
@@ -77,11 +75,8 @@ in {
           description = "Name for the GTK theme";
           default = "adw-gtk3-dark";
         };
-        package = mkOption {
-          type = package;
-          description = "Package providing the GTK theme";
-
-          default = pkgs.adw-gtk3;
+        package = mkPackageOption pkgs "GTK theme" {
+          default = "adw-gtk3";
         };
       };
 
@@ -92,39 +87,8 @@ in {
           default = "MoreWaita";
         };
 
-        package = mkOption {
-          type = package;
-          description = builtins.trace (pins.MoreWaita "The GTK icon theme to be used");
-          default = pkgs.morewaita-icon-theme.overrideAttrs {
-            src = pins.MoreWaita;
-            installPhase = ''
-              runHook preInstall
-
-              install -d $out/share/icons/MoreWaita
-              cp -r . $out/share/icons/MoreWaita
-              cp ${../../../assets/icons/my-caffeine-on-symbolic.svg} $out/share/icons/MoreWaita/symbolic/status/my-caffeine-on-symbolic.svg
-              cp ${../../../assets/icons/my-caffeine-off-symbolic.svg} $out/share/icons/MoreWaita/symbolic/status/my-caffeine-off-symbolic.svg
-              gtk-update-icon-cache -f -t $out/share/icons/MoreWaita && xdg-desktop-menu forceupdate
-
-              runHook postInstall
-            '';
-
-            /*
-            a patch is needed because MoreWaita expects a `meson.build` file in the directory, containing the
-            name of every icon.
-            */
-            patches = singleton (pkgs.writeText "add-caffeine-icons.patch" ''
-              diff --git a/symbolic/status/meson.build b/symbolic/status/meson.build
-              index 4e5bfc5..3bbf989 100644
-              --- a/symbolic/status/meson.build
-              +++ b/symbolic/status/meson.build
-              @@ -17,2 +17,4 @@ regular_files = [
-                   'keepassxc-unlocked.svg',
-              +    'my-caffeine-off-symbolic.svg',
-              +    'my-caffeine-on-symbolic.svg',
-                   'pamac-tray-no-update.svg',
-            '');
-          };
+        package = mkPackageOption pkgs "GTK icon theme" {
+          default = "morewaita-icon-theme";
         };
       };
     };
