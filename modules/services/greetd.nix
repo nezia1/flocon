07641f5b74f1e459db205bd1ae1a99ca2edd2256
Anthony Rodriguez <anthony@nezia.dev>
2025-06-26 14:57:49 +0200

    services/greetd: use cage

--- modules/services/greetd.nix
+++ modules/services/greetd.nix
@@ -2,52 +2,21 @@
   lib,
   pkgs,
   config,
-  myLib,
   ...
-}:
-# thanks https://git.jacekpoz.pl/poz/niksos/src/commit/f8d5e7ccd9c769f7c0b564f10dff419285e75248/modules/services/greetd.nix
-let
-  inherit (lib.meta) getExe getExe';
+}: let
   inherit (lib.modules) mkIf;
 
-  inherit (myLib.generators) toHyprConf;
-
-  hyprland = config.programs.hyprland.package;
-
   styleCfg = config.local.style;
-
-  hyprctl = getExe' hyprland "hyprctl";
-  Hyprland = getExe' hyprland "Hyprland";
-
-  greeter = getExe pkgs.greetd.regreet;
-
-  hyprlandConfig = pkgs.writeText "greetd-hyprland-config" (toHyprConf {
-    attrs = {
-      misc = {
-        disable_hyprland_logo = true;
-        disable_splash_rendering = true;
-      };
-      animations = {
-        enabled = false;
-        first_launch_animation = false;
-      };
-      workspace = "1,default:true,gapsout:0,gapsin:0,border:false,decorate:false";
-      monitor = "HDMI-A-1, disable";
-      exec-once = "[workspace 1;fullscreen;noanim] ${greeter}; ${hyprctl} dispatch exit";
-    };
-  });
 in {
   config = mkIf (config.local.vars.home.desktop.type == "wm") {
-    environment.systemPackages = [pkgs.adwaita-icon-theme]; # add as fallback
     services.greetd = {
       enable = true;
-      settings = {
-        default_session.command = "${Hyprland} --config ${hyprlandConfig}";
-      };
+      settings.terminal.vt = 1;
     };
 
     programs.regreet = {
       enable = true;
+      cageArgs = ["-s" "-m" "last"];
       cursorTheme = {
         inherit (styleCfg.cursors.xcursor) name package;
       };
