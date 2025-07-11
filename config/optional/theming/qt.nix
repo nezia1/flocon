025a3c3c6851cc1d13f35c6e14892544e7948ab6
Anthony Rodriguez <anthony@nezia.dev>
2025-07-11 18:42:02 -0400

    theming/qt: add qt{5,6}ct configuration

--- config/optional/theming/qt.nix
+++ config/optional/theming/qt.nix
@@ -1,9 +1,23 @@
-{pkgs, ...}: let
+{
+  config,
+  pkgs,
+  ...
+}: let
   variant = "mocha";
   accent = "lavender";
   kvantumTheme = pkgs.catppuccin-kvantum.override {
     inherit variant accent;
   };
+
+  qtctConf = {
+    Appearance = {
+      custom_palette = true;
+      icon_theme = config.local.style.gtk.iconTheme.name;
+      standard_dialogs = "xdgdesktopportal";
+      style = "kvantum-dark";
+    };
+  };
+
   ini = pkgs.formats.ini {};
 in {
   qt = {
@@ -18,9 +32,12 @@ in {
   ];
 
   hj.files = {
-    ".config/Kvantum/kvantum.kvconfig".source = ini.generate "kvantum.kvconfig" {
+    ".config/Kvantum/kvantum.kvconfig".source = ini.generate "kvantum-config" {
       General.theme = "catppuccin-mocha-lavender";
     };
+
+    ".config/qt5ct/qt5ct.conf".source = ini.generate "qt5ct-config" qtctConf;
+    ".config/qt6ct/qt6ct.conf".source = ini.generate "qt6ct-config" qtctConf;
 
     # https://discourse.nixos.org/t/catppuccin-kvantum-not-working/43727/14
     ".config/Kvantum/catppuccin-${variant}-${accent}".source = "${kvantumTheme}/share/Kvantum/catppuccin-${variant}-${accent}";
