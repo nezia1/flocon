46e6e20aba04125c4b277432c6cc8a485aa6fb6c
Anthony Rodriguez <anthony@nezia.dev>
2025-07-11 21:37:20 -0400

    parts/modules: export qtct under hjemModules

--- parts/modules.nix
+++ parts/modules.nix
@@ -3,6 +3,7 @@ _: {
     hjemModules = {
       librewolf = ../modules/shared/hjem-rum/librewolf.nix;
       xdg-autostart = ../modules/shared/hjem-rum/xdg/autostart.nix;
+      qtct = ../modules/shared/hjem/misc/qtct.nix;
     };
   };
 }
