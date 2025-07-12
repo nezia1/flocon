3b30de755311d10c894b861c00d3b313c2cbf71d
Anthony Rodriguez <anthony@nezia.dev>
2025-07-12 00:27:54 -0400

    parts/modules: export kvantum under hjemModules

--- parts/modules.nix
+++ parts/modules.nix
@@ -4,6 +4,7 @@ _: {
       librewolf = ../modules/shared/hjem-rum/librewolf.nix;
       xdg-autostart = ../modules/shared/hjem-rum/xdg/autostart.nix;
       qtct = ../modules/shared/hjem/misc/qtct.nix;
+      kvantum = ../modules/shared/hjem/misc/kvantum.nix;
     };
   };
 }
