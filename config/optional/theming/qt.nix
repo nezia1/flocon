f53e9bc3ac405de622d6c35c983eded8bbb63139
Anthony Rodriguez <anthony@nezia.dev>
2025-07-12 00:28:03 -0400

    theming/qt: use kvantum module

--- config/optional/theming/qt.nix
+++ config/optional/theming/qt.nix
@@ -1,30 +1,22 @@
 {
-  lib,
   pkgs,
   config,
   self,
   ...
 }: let
-  inherit (lib.strings) toLower;
-  # TODO: modularize kvantum (taking inspo from https://github.com/rice-cracker-dev/rnc/blob/dd41577374eecf5655b69f554d3cf58474c4157f/modules/home/theme/qtct.nix)
   # TODO: add global state for kvantum/qtct themes
-  variant = "Mocha";
-  accent = "lavender";
-
   kvantumTheme = pkgs.catppuccin-kvantum.override {
-    inherit accent;
-    variant = toLower variant;
+    variant = "mocha";
+    accent = "lavender";
   };
-
   qtctTheme = pkgs.catppuccin-qt5ct;
-
   qtctConf = {
     Appearance = {
       custom_palette = true;
       icon_theme = config.local.style.gtk.iconTheme.name;
       standard_dialogs = "xdgdesktopportal";
       style = "kvantum-dark";
-      color_scheme_path = "${qtctTheme}/share/qt5ct/colors/Catppuccin-${variant}.conf";
+      color_scheme_path = "${qtctTheme}/share/qt5ct/colors/Catppuccin-Mocha.conf";
     };
 
     Interface = {
@@ -34,10 +26,11 @@
       menus_have_icons = true;
     };
   };
-
-  ini = pkgs.formats.ini {};
 in {
-  hjem.extraModules = [self.hjemModules.qtct];
+  hjem.extraModules = [
+    self.hjemModules.kvantum
+    self.hjemModules.qtct
+  ];
 
   qt = {
     enable = true;
@@ -46,18 +39,18 @@ in {
   };
 
   hj = {
-    local.misc.qtct = {
-      enable = true;
-      qt5.settings = qtctConf;
-      qt6.settings = qtctConf;
-    };
-
-    files = {
-      # https://discourse.nixos.org/t/catppuccin-kvantum-not-working/43727/14
-      ".config/Kvantum/catppuccin-${toLower variant}-${accent}".source = "${kvantumTheme}/share/Kvantum/catppuccin-${toLower variant}-${accent}";
-
-      ".config/Kvantum/kvantum.kvconfig".source = ini.generate "kvantum-config" {
-        General.theme = "catppuccin-mocha-lavender";
+    local.misc = {
+      qtct = {
+        enable = true;
+        qt5.settings = qtctConf;
+        qt6.settings = qtctConf;
+      };
+      kvantum = {
+        enable = true;
+        theme = {
+          name = "catppuccin-mocha-lavender";
+          package = kvantumTheme;
+        };
       };
     };
   };
