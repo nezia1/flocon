2c9dff91958274b41c6d2849277d029e77f0283e
Anthony Rodriguez <anthony@nezia.dev>
2025-07-14 00:44:21 -0400

    services/brightness: remove conditional toggling

--- config/optional/services/brightness.nix
+++ config/optional/services/brightness.nix
@@ -1,11 +1,3 @@
-{
-  lib,
-  config,
-  ...
-}: let
-  inherit (lib.modules) mkIf;
-in {
-  config = mkIf (config.local.profiles.laptop.enable && config.local.vars.home.desktop.type == "wm") {
-    hardware.brillo.enable = true;
-  };
+_: {
+  hardware.brillo.enable = true;
 }
