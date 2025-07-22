d7cacc6f8a641814dfaf5d544ea86647879e3e2d
Anthony Rodriguez <anthony@nezia.dev>
2025-07-22 15:46:01 +0200

    solaire: setup monitors

---
+++ hosts/solaire/monitors.nix
@@ -0,0 +1,15 @@
+_: {
+  local.monitors = [
+    {
+      name = "DP-1";
+      width = 2560;
+      height = 1440;
+      primary = true;
+    }
+    {
+      name = "HDMI-A-1";
+      width = 1920;
+      height = 1080;
+    }
+  ];
+}
