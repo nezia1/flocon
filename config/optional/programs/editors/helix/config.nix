e24949d15d1db4c5a53450e70594a424e399be52
Anthony Rodriguez <anthony@nezia.dev>
2025-07-24 10:04:05 +0200

    editors/helix: wrap dependencies
    Instead of doing the unwieldy manual LSP config, we can simply make them
    available from within helix! That works amazing for builtins, and if we
    ever wanna add another lsp, we can just configure it without having to
    use `getExe`, since it will be available in PATH!

--- config/optional/programs/editors/helix/config.nix
+++ config/optional/programs/editors/helix/config.nix
@@ -5,17 +5,40 @@
   pins,
   ...
 }: let
-  inherit (lib.meta) getExe;
+  inherit (builtins) attrValues;
+  inherit (lib.strings) getName getVersion makeBinPath;
 
   settingsFormat = pkgs.formats.toml {};
   theme = styleCfg.colors.scheme {
     templateRepo = pins.base16-helix;
     use-ifd = "auto";
   };
+
+  helix = pkgs.helix;
+  helixWrapped = let
+    extraPackages = attrValues {
+      inherit
+        (pkgs)
+        nixd
+        alejandra
+        ;
+    };
+  in
+    pkgs.symlinkJoin {
+      name = "${getName helix}-wrapped-${getVersion helix}";
+      paths = [helix];
+      preferLocalBuild = true;
+      nativeBuildInputs = [pkgs.makeWrapper];
+      postBuild = ''
+        wrapProgram $out/bin/hx \
+          --prefix PATH : ${makeBinPath extraPackages}
+      '';
+    };
+
   styleCfg = config.local.style;
 in {
   hj = {
-    packages = [pkgs.helix];
+    packages = [helixWrapped];
     files = {
       ".config/helix/config.toml".source = settingsFormat.generate "helix-config" {
         theme = "base16";
@@ -32,38 +55,22 @@ in {
         };
       };
 
-      ".config/helix/languages.toml".source = pkgs.concatText "helix-languages" [
-        (settingsFormat.generate "helix-use-grammars" {
-          use-grammars = {only = ["nix"];};
-        })
-        (settingsFormat.generate "helix-language-config" {
-          language = [
-            {
-              name = "nix";
-              language-servers = ["nixd"];
-              formatter.command = getExe pkgs.alejandra;
-              auto-format = true;
-            }
+      ".config/helix/languages.toml".source = settingsFormat.generate "helix-language-config" {
+        language = [
+          {
+            name = "nix";
+            language-servers = ["nixd"];
+            formatter.command = "alejandra";
+            auto-format = true;
+          }
+        ];
+        language-server = {
+          nixd.args = [
+            "--semantic-tokens"
+            "--inlay-hints"
           ];
-          language-server = {
-            nixd = {
-              command = getExe pkgs.nixd;
-              args = [
-                "--semantic-tokens"
-                "--inlay-hints"
-              ];
-            };
-          };
-        })
-        (settingsFormat.generate "helix-grammar-sources" {
-          grammar = [
-            {
-              name = "nix";
-              source.path = pkgs.tree-sitter-grammars.tree-sitter-nix;
-            }
-          ];
-        })
-      ];
+        };
+      };
 
       ".config/helix/themes/base16.toml".source = theme;
     };
