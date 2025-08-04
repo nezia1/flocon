6d648e6dd8d202349dc5170a47966b35a2aaff60
Anthony Rodriguez <anthony@nezia.dev>
2025-08-04 02:03:32 +0200

    services/docker: add user to group; enable dns for podman-compose

--- config/optional/services/docker.nix
+++ config/optional/services/docker.nix
@@ -1,6 +1,10 @@
-_: {
+{config, ...}: let
+  inherit (config.local.vars.system) username;
+in {
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
+    defaultNetwork.settings.dns_enabled = true;
   };
+  users.users.${username}.extraGroups = ["podman"];
 }
