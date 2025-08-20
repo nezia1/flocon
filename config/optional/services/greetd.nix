71accd68ec28d1b92be69f4070d3f9ef14076d02
Anthony Rodriguez <anthony@nezia.dev>
2025-08-20 14:01:30 +0200

    services/greetd: remove TODO comment

--- config/optional/services/greetd.nix
+++ config/optional/services/greetd.nix
@@ -10,7 +10,6 @@
   inherit (lib.strings) escapeShellArgs;
   primaryMonitor = findFirst (m: m.primary) null config.local.monitors;
 
-  # TODO: setup monitors with NixOS options, so that this may be setup for the main monitor of both desktop and laptop
   run-regreet = pkgs.writeShellScript "run-regreet" ''
     ${getExe pkgs.wlr-randr} \
       --output ${primaryMonitor.name} \
