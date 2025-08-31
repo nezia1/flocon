81ec55b281226eb202f31d46d04f8e8031f932e0
Anthony Rodriguez <anthony@nezia.dev>
2025-08-31 19:25:23 +0200

    services/ssh: use ksshaskpass

--- config/optional/services/ssh.nix
+++ config/optional/services/ssh.nix
@@ -1,5 +1,15 @@
-_: {
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib.meta) getExe;
+in {
   hj = {
+    environment.sessionVariables = {
+      SSH_ASKPASS = getExe pkgs.kdePackages.ksshaskpass;
+      SSH_ASKPASS_REQUIRE = "prefer";
+    };
     files = {
       ".ssh/config".text = ''
         AddKeysToAgent yes
