97df1308d3c0223c2d9de259ba7c6b2d62dafd4b
Anthony Rodriguez <anthony@nezia.dev>
2025-09-16 15:46:10 +0200

    core/networking: move back to wpa_supplicant for NetworkManager backend

--- config/optional/core/networking.nix
+++ config/optional/core/networking.nix
@@ -5,15 +5,9 @@
     networkmanager = {
       enable = true;
       wifi = {
-        backend = "iwd";
         powersave = false;
       };
       dns = "systemd-resolved";
-    };
-    wireless.iwd.settings = {
-      Network.EnableIPv6 = true;
-      Settings.AutoConnect = true;
-      General.AddressRandomization = "network";
     };
   };
   services.resolved = {
