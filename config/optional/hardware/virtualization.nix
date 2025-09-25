2c00e1393560700da31c032be5d341453d8faf5b
Anthony Rodriguez <anthony@nezia.dev>
2025-09-25 13:28:00 +0200

    hardware/virtualization: add spice USB redirection and folder sharing

--- config/optional/hardware/virtualization.nix
+++ config/optional/hardware/virtualization.nix
@@ -1,4 +1,8 @@
-{
-  virtualisation.libvirtd.enable = true;
+{pkgs, ...}: {
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu.vhostUserPackages = [pkgs.virtiofsd];
+  };
+  virtualisation.spiceUSBRedirection.enable = true;
   programs.virt-manager.enable = true;
 }
