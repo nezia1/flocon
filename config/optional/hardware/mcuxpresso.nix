926292f09d7c6cf444df00c19d9bc79ca558ea88
Anthony Rodriguez <anthony@nezia.dev>
2025-09-29 14:38:28 +0200

    hardware/mcuxpresso: add udev package back
    MCUXpresso's binary file has been added manually to the binary cache, so
    this can now build with the CI.

--- config/optional/hardware/mcuxpresso.nix
+++ config/optional/hardware/mcuxpresso.nix
@@ -1,4 +1,4 @@
-{...}: {
+{self', ...}: {
   # This makes CI fail because of `requireFile`
-  # services.udev.packages = [self'.packages.mcuxpressoide];
+  services.udev.packages = [self'.packages.mcuxpressoide];
 }
