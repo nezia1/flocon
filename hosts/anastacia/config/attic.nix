c8c6dfadbf04e6220bd73b294d9f0beb89029791
Anthony Rodriguez <anthony@nezia.dev>
2025-09-30 01:25:23 +0200

    anastacia/attic: init attic

---
+++ hosts/anastacia/config/attic.nix
@@ -0,0 +1,84 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  port = "8080";
+in {
+  age.secrets = {
+    attic-env.file = ../../../secrets/attic-env.age;
+  };
+
+  users = {
+    users.atticd = {
+      isSystemUser = true;
+      group = "atticd";
+    };
+    groups.atticd = {};
+  };
+
+  services = {
+    atticd = {
+      enable = true;
+      environmentFile = config.age.secrets.attic-env.path;
+
+      settings = {
+        listen = "[::]:${port}";
+
+        jwt = {};
+
+        database.url = "postgresql:///atticd?host=/run/postgresql";
+
+        garbage-collection = {
+          interval = "12 hours";
+          default-retention-period = "6 months";
+        };
+
+        storage = {
+          type = "s3";
+          endpoint = "hel1.your-objectstorage.com";
+          region = "eu-central";
+          bucket = "nezia";
+        };
+
+        # Data chunking
+        #
+        # Warning: If you change any of the values here, it will be
+        # difficult to reuse existing chunks for newly-uploaded NARs
+        # since the cutpoints will be different. As a result, the
+        # deduplication ratio will suffer for a while after the change.
+        chunking = {
+          # The minimum NAR size to trigger chunking
+          #
+          # If 0, chunking is disabled entirely for newly-uploaded NARs.
+          # If 1, all NARs are chunked.
+          nar-size-threshold = 64 * 1024; # 64 KiB
+
+          # The preferred minimum size of a chunk, in bytes
+          min-size = 16 * 1024; # 16 KiB
+
+          # The preferred average size of a chunk, in bytes
+          avg-size = 64 * 1024; # 64 KiB
+
+          # The preferred maximum size of a chunk, in bytes
+          max-size = 256 * 1024; # 256 KiB
+        };
+      };
+    };
+
+    postgresql = {
+      enable = true;
+      ensureUsers = [
+        {
+          name = config.services.atticd.user;
+          ensureDBOwnership = true;
+        }
+      ];
+      ensureDatabases = ["atticd"];
+    };
+
+    caddy.virtualHosts."attic.nezia.dev".extraConfig = ''
+      reverse_proxy ${config.services.atticd.settings.listen}
+    '';
+  };
+}
