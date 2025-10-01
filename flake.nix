64c7bb5acede8f60754feba7324fd8607b2bde91
Anthony Rodriguez <anthony@nezia.dev>
2025-10-01 03:10:48 +0200

    flake: switch to new public key for attic

--- flake.nix
+++ flake.nix
@@ -189,7 +189,7 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
-      "nezia-flocon:v4f4Cui0+AQl0vNavzrDRZGh70Qtc9EBvDCT1BGdadM="
+      "nezia-flocon:r8LaMa6W9jFMkm9R4As2wxcf9jxh/8AmmbfkHge8Y9c="
     ];
   };
 }
