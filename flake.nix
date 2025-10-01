3909160a53def5d6ef2aa263e8a1f29a3b88e81d
Anthony Rodriguez <anthony@nezia.dev>
2025-10-01 13:59:45 +0200

    flake: update cache public key (again!!)

--- flake.nix
+++ flake.nix
@@ -189,7 +189,7 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
-      "nezia-flocon:ScU3QNkIoBSmq9BTDSzQgAZXJH2AUnJ5+vdpaNocfjM="
+      "nezia-flocon:BqUGJqk5PPXwRz9fBhL9tmGQzYEVdwtEw3bDErg2BLM="
     ];
   };
 }
