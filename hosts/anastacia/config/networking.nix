dad2ac2c416e8874110247620b737f4627306c89
Anthony Rodriguez <anthony@nezia.dev>
2025-10-03 19:55:58 +0200

    hosts/anastacia: add networking settings

---
+++ hosts/anastacia/config/networking.nix
@@ -0,0 +1,41 @@
+{
+  networking = {
+    useNetworkd = true;
+    useDHCP = false;
+
+    nameservers = [
+      "192.168.1.1"
+      "1.1.1.1"
+      "2606:4700:4700::1111"
+    ];
+
+    defaultGateway = {
+      address = "192.168.1.1";
+      interface = "eth0";
+    };
+
+    defaultGateway6 = {
+      address = "fe80::1adf:26ff:feb1:2090";
+      interface = "eth0";
+    };
+
+    interfaces.eth0 = {
+      ipv4.addresses = [
+        {
+          address = "192.168.1.50";
+          prefixLength = 24;
+        }
+      ];
+      ipv6.addresses = [
+        {
+          address = "2a01:cb15:149:dd00::50";
+          prefixLength = 64;
+        }
+      ];
+    };
+  };
+
+  services.udev.extraRules = ''
+    ATTR{address}=="78:55:36:02:70:26", NAME="eth0"
+  '';
+}
