b2cb8bf6f4f8070fbfd18f504488c5feba79ff7e
Anthony Rodriguez <anthony@nezia.dev>
2025-10-05 01:14:35 +0200

    flake: remove attic from caches

--- flake.nix
+++ flake.nix
@@ -181,7 +181,6 @@
       "https://hyprland.cachix.org"
       "https://nix-gaming.cachix.org"
       "https://anyrun.cachix.org"
-      "https://attic.nezia.dev/nezia-flocon"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
@@ -189,7 +188,6 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
-      "nezia-flocon:BqUGJqk5PPXwRz9fBhL9tmGQzYEVdwtEw3bDErg2BLM="
     ];
   };
 }
