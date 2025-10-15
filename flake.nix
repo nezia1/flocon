6a4652b2b1b615dcdeda02a50bce8b79fc969942
Anthony Rodriguez <anthony@nezia.dev>
2025-10-15 12:45:18 +0200

    flake: revert hjem-rum to last working commit

--- flake.nix
+++ flake.nix
@@ -69,8 +69,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # FIXME: back to upstream when https://github.com/snugnug/hjem-rum/pull/140 gets merged
     hjem-rum = {
-      url = "github:snugnug/hjem-rum";
+      url = "github:snugnug/hjem-rum/7c931af837c79d03275188e874e03ede10ce6183";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
