19361c65896dcfbfd4694cd262ccaad123efcda5
Anthony Rodriguez <anthony@nezia.dev>
2025-10-15 11:53:22 +0200

    programs/hyprland: fix anyrun toggle bind

--- config/optional/programs/hypr/land/binds.nix
+++ config/optional/programs/hypr/land/binds.nix
@@ -1,17 +1,30 @@
-{lib, ...}: let
+{
+  pkgs,
+  lib,
+  ...
+}: let
   inherit (builtins) substring;
 
   # thanks https://github.com/fufexan/dotfiles/blob/c0b3c77d95ce1f574a87e7f7ead672ca0d951245/home/programs/wayland/hyprland/binds.nix#L16-L20
   toggle = program: app2unit: let
     prog = substring 0 14 program;
-  in "pkill ${prog} || ${lib.optionalString app2unit "app2unit -- "} ${program}";
+  in "pkill '^${prog}\$' || ${lib.optionalString app2unit "app2unit -- "} ${program}";
   runOnce = program: "pgrep ${program} || app2unit -- ${program}";
   run = program: "app2unit -- ${program}";
+
+  toggleAnyrun = pkgs.writeShellScript "toggle-anyrun" ''
+    if pgrep -f '/bin/anyrun$'; then
+      anyrun close
+      else
+        anyrun
+    fi
+
+  '';
 in {
   hj.rum.desktops.hyprland.settings = {
     "$mod" = "SUPER";
     bindr = [
-      "$mod, SUPER_L, exec, ${toggle "anyrun" false}"
+      "$mod, SUPER_L, exec, ${toggleAnyrun}"
     ];
     bind = [
       "$mod, Return, exec, ${run "footclient"}"
