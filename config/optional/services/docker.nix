82709a3f40d5c4c6be557dc1a1a08886e83ff006
Anthony Rodriguez <anthony@nezia.dev>
2025-11-11 12:47:25 +0100

    services/docker: add docker compose

--- config/optional/services/docker.nix
+++ config/optional/services/docker.nix
@@ -1,10 +1,17 @@
-{config, ...}: let
+{
+  config,
+  pkgs,
+  ...
+}: let
   inherit (config.local.vars.system) username;
 in {
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
     defaultNetwork.settings.dns_enabled = true;
+  };
+  hj = {
+    packages = [pkgs.docker-compose];
   };
   users.users.${username}.extraGroups = ["podman"];
 }
