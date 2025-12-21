4ec359a9c19dddf02fd0fc15cb47cf07212b7a6f
Anthony Rodriguez <anthony@nezia.dev>
2025-12-21 21:15:11 +0100

    services/pipewire: update sampling rates and low latency setup
    services/pipewire: update sampling rates and low latency setup

--- config/optional/services/pipewire.nix
+++ config/optional/services/pipewire.nix
@@ -1,11 +1,44 @@
-{inputs, ...}: {
-  imports = [
-    inputs.nix-gaming.nixosModules.pipewireLowLatency
-  ];
-
-  services = {
-    pulseaudio.enable = false;
-  };
-  # rtkit is optional but recommended
+let
+  rate = 48000;
+  quantum = 64;
+  qr = "${toString quantum}/${toString rate}";
+in {
   security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+
+    extraConfig = {
+      pipewire."92-low-latency" = {
+        "context.properties" = {
+          "default.clock.rate" = rate;
+          "default.clock.quantum" = quantum;
+          "default.clock.min-quantum" = quantum;
+          "default.clock.max-quantum" = quantum;
+        };
+      };
+
+      pipewire-pulse."92-low-latency" = {
+        "context.properties" = [
+          {
+            name = "libpipewire-module-protocol-pulse";
+            args = {};
+          }
+        ];
+        "pulse.properties" = {
+          "pulse.min.req" = qr;
+          "pulse.default.req" = qr;
+          "pulse.max.req" = qr;
+          "pulse.min.quantum" = qr;
+          "pulse.max.quantum" = qr;
+        };
+        "stream.properties" = {
+          "node.latency" = qr;
+          "resample.quality" = 1;
+        };
+      };
+    };
+  };
 }
