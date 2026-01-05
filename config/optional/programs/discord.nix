5f7b0c1005bccc359785e4fd6cb1607de4802734
Anthony Rodriguez <anthony@nezia.dev>
2026-01-05 12:59:46 +0100

    programs/discord: use vesktop

--- config/optional/programs/discord.nix
+++ config/optional/programs/discord.nix
@@ -1,19 +1,8 @@
-{
-  lib,
-  pkgs,
-  inputs,
-  ...
-}: let
-  pkgsDiscord = import inputs.nixpkgs-discord-krisp-fix {
-    system = pkgs.stdenv.hostPlatform.system;
-    config.allowUnfree = true;
+{pkgs, ...}: let
+  discord = pkgs.vesktop.override {
+    withTTS = false;
+    withSystemVencord = true;
   };
-  discord =
-    (pkgsDiscord.discord.overrideAttrs {
-      inherit (pkgs.discord) version src;
-    }).override {
-      # TODO: fix OpenASAR / Vencord / Moonlight being wrong in upstream PR (cd doesnt go into correct dir)
-    };
 in {
   config = {
     hj = {
