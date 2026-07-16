c777deb4d87e1084bc292ab638f8807d31f2a70a
Anthony Rodriguez <anthony@nezia.dev>
2026-07-16 12:47:51 +0200

    services/kmscon: switch to new api

--- config/optional/services/kmscon.nix
+++ config/optional/services/kmscon.nix
@@ -1,17 +1,12 @@
-{pkgs, ...}: {
+{
   config = {
     services.kmscon = {
       enable = true;
-      fonts = [
-        {
-          name = "0xProto Nerd Font";
-          package = pkgs.nerd-fonts._0xproto;
-        }
-      ];
-      extraConfig = ''
-        font-size=13
-        font-dpi=144
-      '';
+      config = {
+        font-name = "0xProto Nerd Font";
+        font-size = 13;
+        font-dpi = 144;
+      };
     };
   };
 }
