7ce912d19512cf158ad044001dcdc13631bd5c97
Anthony Rodriguez <anthony@nezia.dev>
2026-07-16 12:48:55 +0200

    flake: switch hjem-rum to main

--- flake.nix
+++ flake.nix
@@ -85,9 +85,8 @@
 
     hjem.follows = "hjem-rum/hjem";
 
-    # FIXME: back to upstream when https://github.com/snugnug/hjem-rum/pull/140 gets merged
     hjem-rum = {
-      url = "github:snugnug/hjem-rum/pull/140/head";
+      url = "github:snugnug/hjem-rum";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
