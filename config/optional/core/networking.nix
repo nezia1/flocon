b72f74c300adae999d6c69b4e80f2c916608636a
Anthony Rodriguez <anthony@nezia.dev>
2026-07-20 01:34:23 +0200

    core/networking: use default nameservers (custom DNS is setup through tailscale)

--- config/optional/core/networking.nix
+++ config/optional/core/networking.nix
@@ -1,7 +1,6 @@
 {config, ...}: {
   networking = {
     inherit (config.local.vars.system) hostName;
-    nameservers = ["1.1.1.1" "1.0.0.1"];
     networkmanager = {
       enable = true;
       wifi = {
@@ -10,10 +9,5 @@
       dns = "systemd-resolved";
     };
   };
-  services.resolved = {
-    enable = true;
-    settings = {
-      Resolve.DnsOverTLS = "opportunistic";
-    };
-  };
+  services.resolved.enable = true;
 }
